6c4969e32ec2fa4e7c5569303e3cf4e8c0a6b1a1
Jon West <electrikjesus@gmail.com>
2023-07-10 10:55:55 -0400

    Allow to override low_mem detection
    Change-Id: I62c70ab925c2dabb9ea6218e179363eb68b1cf79

--- init.sh
+++ init.sh
@@ -602,7 +602,7 @@ function set_lowmem()
 
 	if [ "$mem_size" -le "$SIZE_512MB" ]
 	then
-		setprop ro.config.low_ram true
+		setprop ro.config.low_ram ${FORCE_LOW_MEM:-true}
 	else
 		setprop ro.config.low_ram false
 	fi
