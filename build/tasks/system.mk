b1e3a2604fdd5411f892c22081938b04c8806f5e
Erfan Abdi <erfangplus@gmail.com>
2023-08-02 19:00:30 +0000

    common: Pack sfs and erofs by aosp
    Change-Id: I66e9e066871a7a0bc0a4b406832fd882b220637c

--- build/tasks/system.mk
+++ build/tasks/system.mk
@@ -3,12 +3,3 @@ $(PRODUCT_OUT)/build.prop: $(INSTALLED_BUILD_PROP_TARGET) $(INSTALLED_VENDOR_BUI
 
 $(BUILT_SYSTEMIMAGE): $(PRODUCT_OUT)/build.prop
 
-ifneq ($(MKSQUASHFS),)
-$(systemimg): $(BUILT_SYSTEMIMAGE) | $(MKSQUASHFS)
-	$(call build-squashfs-target,$^,$@)
-endif
-
-ifneq ($(MKEROFS),)
-$(systemimg): $(BUILT_SYSTEMIMAGE) | $(MKEROFS)
-	$(call build-erofs-target,$^,$@)
-endif
