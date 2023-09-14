693fcb4a96e6e9aa4daa6aeb75d21741b86dad65
Erfan Abdi <erfangplus@gmail.com>
2023-09-14 23:52:35 +0000

    common: Build ext4 and pack it inside sfs or efs
    Change-Id: I511fe69c9ef4481e968156972a7bcbe398507e66

--- build/tasks/system.mk
+++ build/tasks/system.mk
@@ -3,3 +3,12 @@ $(PRODUCT_OUT)/build.prop: $(INSTALLED_BUILD_PROP_TARGET) $(INSTALLED_VENDOR_BUI
 
 $(BUILT_SYSTEMIMAGE): $(PRODUCT_OUT)/build.prop
 
+ifneq ($(MKSQUASHFS),)
+$(systemimg): $(BUILT_SYSTEMIMAGE) | $(MKSQUASHFS)
+	$(call build-squashfs-target,$^,$@)
+endif
+
+ifneq ($(MKEROFS),)
+$(systemimg): $(BUILT_SYSTEMIMAGE) | $(MKEROFS)
+	$(call build-erofs-target,$^,$@)
+endif
