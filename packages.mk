71beb1394e264cbe8e3a3749709d2232ae640de6
Yumi Yukimura <me.cafebabe@gmail.com>
2024-07-24 14:29:27 +0800

    x86:common: Run DHCP client in recovery
    - Import dhcpclient & all required changes
    
    Change-Id: I657ed3cc77c780a937a5eb762b5838689877bf20

--- packages.mk
+++ packages.mk
@@ -165,3 +165,8 @@ PRODUCT_PACKAGES += iptsd_runner iptsd \
                     iptsd-dump \
                     iptsd-perf
 endif
+
+# For Recovery
+## DHCP client
+PRODUCT_PACKAGES += \
+    x86_dhcpclient.recovery
