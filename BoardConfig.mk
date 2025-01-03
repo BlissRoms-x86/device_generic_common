2ad079cad623072724c56364083a6d4be5c1af43
hmtheboy154 <buingoc67@gmail.com>
2025-01-02 22:52:17 -0500

    Commonize the bootctrl HAL
    Why "commonize" ? It's because the current bootctrl HAL is focusing on
    supporting Grub. From the file name to the place where it read the config
    to alter it all contain "grub" on it.
    
    Renaming files & functions to not mentioning Grub again. Also allow specifying
    which file to read using a property.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,7 +20,7 @@ AB_OTA_PARTITIONS += \
 	ramdisk-recovery
 
 # Rootfs
-BOARD_ROOT_EXTRA_FOLDERS := grub
+BOARD_ROOT_EXTRA_FOLDERS := boot
 
 # Some framework code requires this to enable BT
 BOARD_HAVE_BLUETOOTH := true
