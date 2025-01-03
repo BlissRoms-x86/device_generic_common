3d7dceccef005182f5118119265e4ca9b3bd9a50
hmtheboy154 <buingoc67@gmail.com>
2025-01-02 23:11:51 -0500

    BoardConfig: sepolicy should be append, not define
    Turned out there are some policy being defined on build/core so if we
    add our own, we should append those previous one.
    
    This fixed one of the weird issue on recovery that it suddenly abort the
    OTA flashing.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,8 +173,8 @@ BOARD_SEPOLICY_DIRS += device/generic/common/sepolicy/nonplat \
                        vendor/intel/proprietary/houdini/sepolicy \
                        vendor/google/proprietary/widevine-prebuilt/sepolicy
 
-BOARD_PLAT_PRIVATE_SEPOLICY_DIR := device/generic/common/sepolicy/plat_private
-BOARD_VENDOR_SEPOLICY_DIRS := device/generic/common/sepolicy/vendor
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += device/generic/common/sepolicy/plat_private
+BOARD_VENDOR_SEPOLICY_DIRS += device/generic/common/sepolicy/vendor
 
 BOARD_BUILD_SYSTEM_ROOT_IMAGE := true
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 4718592000
