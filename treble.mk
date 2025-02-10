1ce4228c8c62926f7a390ea03b8303fbce483277
hmtheboy154 <buingoc67@gmail.com>
2025-02-10 14:19:51 -0500

    Include Emulated Camera HAL and a cmdline option to enable it
    Because libvintf doesn't allow 2 xml defining the same service,
    regardless of same or different version, we have to clone Google's
    camera.provider@2.7 and downgrade it to camera.provider@2.5.
    
    This may useful for those who need to use app that require a camera
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- treble.mk
+++ treble.mk
@@ -106,5 +106,18 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@2.0-service.intel
 
+PRODUCT_SOONG_NAMESPACES += \
+    hardware/google/camera \
+    hardware/google/camera/devices/EmulatedCamera \
+
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider@2.5-service-google \
+    libgooglecamerahwl_impl
+
+PRODUCT_COPY_FILES += \
+    hardware/google/camera/devices/EmulatedCamera/hwl/configs/emu_camera_back.json:$(TARGET_COPY_OUT_VENDOR)/etc/config/emu_camera_back.json \
+    hardware/google/camera/devices/EmulatedCamera/hwl/configs/emu_camera_front.json:$(TARGET_COPY_OUT_VENDOR)/etc/config/emu_camera_front.json \
+    hardware/google/camera/devices/EmulatedCamera/hwl/configs/emu_camera_depth.json:$(TARGET_COPY_OUT_VENDOR)/etc/config/emu_camera_depth.json
+
 # vndservicemanager
 PRODUCT_PACKAGES += vndservicemanager
