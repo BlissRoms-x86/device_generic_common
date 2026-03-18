30e6ee332c9952e601e5ee231a0f5aa27d2f1090
hmtheboy154 <buingoc67@gmail.com>
2026-03-18 04:03:00 -0400

    Change how we fetch usb.ids & pci.ids
    Using git submodules is a pretty time consuming way to maintain
    these files. So we just get a script that automatically download
    them instead. Also instead of using PRODUCT_COPY_FILES we will
    write a dedicated Android.bp.
    
    Signed-off-by: hmtheboy154 <buingoc67@gmail.com>

--- packages.mk
+++ packages.mk
@@ -137,6 +137,9 @@ PRODUCT_PACKAGES += \
     libaptX_encoder \
     libaptXHD_encoder
 
+# usb.ids & pci.ids
+PRODUCT_PACKAGES += usb.ids pci.ids
+
 ## Enable hidden features on Android
 PRODUCT_PACKAGES += \
 	pc.xml \
